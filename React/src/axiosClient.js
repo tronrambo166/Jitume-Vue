1ee7a3e5f8cee4ced0e75fafd175fb14edce2236
Malan Español <tronrambo166@gmail.com>
2025-01-10 18:31:00 +0600

    updated

--- React/src/axiosClient.js
+++ React/src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-     baseURL: "tujitume.com/api", 
+     baseURL: "http://tujitume.com/api", 
      //baseURL: "http://127.0.0.1:8000/api",
 });
 
