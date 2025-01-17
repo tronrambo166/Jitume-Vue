bf6140de050e52575f767fe4bf6191263b53dc29
owen <owensmith31588@gmail.com>
2025-01-17 14:04:59 +0300

    update MyInvest.jsx

--- React/src/components/dashboard/Service/MyInvest.jsx
+++ React/src/components/dashboard/Service/MyInvest.jsx
@@ -1,35 +1,30 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axiosClient from "../../../axiosClient";
-import ServEditModal from "./ServEditModal";
 import { useAlert } from "../../partials/AlertContext";
+import ReusableTable from "../business/ReusableTable";
+import { FaChartLine } from "react-icons/fa";
 
 const MyInvest = () => {
-    const [business, setBusiness] = useState([]);
-    const [service, setService] = useState([]);
     const [myInvest, setMyInvest] = useState([]);
-    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-    const [selectedService, setSelectedService] = useState(null); // To track the selected service for editing
     const { showAlert } = useAlert(); // Destructuring showAlert from useAlert
 
     useEffect(() => {
-        const getBusinessAndServices = () => {
+        const getInvestments = () => {
             setTimeout(() => {
                 axiosClient
                     .get("/business/dashhome")
                     .then(({ data }) => {
-                        setBusiness(data.business);
-                        setService(data.services);
                         setMyInvest(data.results);
-                        console.log("My Invest", data);
                     })
                     .catch((err) => {
-                        console.log(err);
+                        console.error(err);
                     });
             }, 500);
         };
-        getBusinessAndServices();
+        getInvestments();
     }, []);
+
     const openEditModal = (service) => {
         setSelectedService(service); // Set the service being edited
         setIsEditModalOpen(true); // Open the modal
@@ -38,193 +33,100 @@ const MyInvest = () => {
     const handleDelete = (id) => {
         axiosClient
             .get("/business/delete_service/" + id)
-            .then(({ data }) => {
-                setService(service.filter((item) => item.id !== id));
-                showAlert("success", "Service deleted successfully.");
+            .then(() => {
+                setMyInvest(myInvest.filter((item) => item.id !== id));
+                showAlert("success", "Investment deleted successfully.");
             })
             .catch((err) => {
-                console.log(err);
+                console.error(err);
             });
     };
 
+    // Define headers for ReusableTable
+    const headers = [
+        "Name",
+        "Category",
+        "Value Needed",
+        "Contact",
+        "Amount",
+        "Business Share",
+        "My Share",
+        "Total Shares",
+        "Status",
+        "Action",
+    ];
+
+    // Map data for ReusableTable
+    const tableData = myInvest.map((item) => ({
+        name: item.name,
+        category: item.category,
+        "value needed": item.investment_needed,
+        contact: item.contact,
+        amount: item.amount,
+        "business share": `${item.share}%`,
+        "my share": `${item.myShare.toFixed()}%`,
+        "total shares": (
+            <p className="text-green-500 font-bold text-center">
+                {/* ! You will update here to get real data from the api  */}
+                {item.totalShares || 90}
+            </p>
+        ),
+        status: (
+            <p className="text-green-500 font-bold text-center">
+                {item.status}
+            </p>
+        ),
+        action: (
+            <Link to={`/business-milestones/${btoa(btoa(item.id))}`}>
+                <button className="text-yellow-500 border border-green-500 rounded-lg py-1 px-3 text-xs">
+                    View milestones
+                </button>
+            </Link>
+        ),
+    }));
+
     return (
-        <div className="py-4">
-            <section className="bg-white border mt-4 rounded-xl w-full px-4 py-6 sm:px-8">
+        <div className="py-4 mt-8 lg:mt-0 px-0 sm:px-[21px]">
+            <section className="bg-white border rounded-xl w-full">
                 {myInvest.length > 0 ? (
-                    <>
-                        <h1 className="text-[#2D3748] font-semibold text-xl sm:text-2xl mb-6">
+                    <section>
+                        <h1 className="text-[#2D3748] ml-6 mt-6 font-semibold text-xl sm:text-2xl mb-6">
                             My Investments
                         </h1>
-                        <div className="overflow-x-auto">
-                            <table className="min-w-full divide-y divide-gray-200">
-                                <thead className="bg-gray-100">
-                                    <tr className="text-gray-500">
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Name
-                                        </th>
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Category
-                                        </th>
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Value Needed
-                                        </th>
-
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Contact
-                                        </th>
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Amount
-                                        </th>
-
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Business Share
-                                        </th>
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            My Share
-                                        </th>
-                                        <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                            Status
-                                        </th>
-                                        <th className="px-4 py-2 text-center text-xs font-medium uppercase tracking-wider">
-                                            Action
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {myInvest.map((item, index) => (
-                                        <tr key={index}>
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.name}
-                                            </td>
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.category}
-                                            </td>
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.investment_needed}
-                                            </td>
-
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.contact}
-                                            </td>
-
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.amount}
-                                            </td>
-
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.share}%
-                                            </td>
-                                            <td className="px-4 py-2 text-sm">
-                                                {item.myShare.toFixed()}%
-                                            </td>
-                                            <td className="px-4 py-2 text-sm">
-                                                <p className="text-green-500 font-bold"> {item.status} </p>
-                                            </td>
-                                            <td className="px-4 py-2 text-center text-sm">
-                                                <Link
-                                                    to={`/business-milestones/${btoa(
-                                                        btoa(item.id)
-                                                    )}`}
-                                                >
-                                                    <button className="text-yellow-500 border border-green-500 rounded-lg py-1 px-3 text-xs">
-                                                        View milestones
-                                                    </button>
-                                                </Link>
-                                            </td>
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
+                        <div className="whitespace-nowrap justify-center">
+                            <ReusableTable
+                                headers={headers}
+                                data={tableData}
+                                rowsPerPage={5}
+                                tableId="myInvestTable"
+                            />
                         </div>
-                    </>
+                    </section>
                 ) : (
-                    <div className="text-gray-500 text-center">
-                        You have no investments.
-                    </div>
+                    <section className="bg-white border border-gray-300 rounded-xl w-full py-6 px-6">
+                        <div className="flex flex-col items-center">
+                            {/* Investment Icon */}
+                            <FaChartLine
+                                size={30}
+                                className="text-gray-500 mb-4"
+                            />
+                            <h3 className="text-[#2D3748] font-semibold text-xl sm:text-l mb-4">
+                                No Investments Found
+                            </h3>
+                            <p className="text-gray-600 text-center">
+                                You don't have any investments yet. Please start
+                                investing to see your portfolio grow.
+                                {/* <Link to="/dashboard/add-investment">
+                                    <span className="text-green font-bold hover:underline">
+                                        {" "}
+                                        Add Investment
+                                    </span>
+                                </Link> */}
+                            </p>
+                        </div>
+                    </section>
                 )}
             </section>
-
-            {/* My Businesses Section */}
-            {/* <section className="bg-white shadow-md rounded-lg mb-6 p-4">
-                <h1 className="text-[#2D3748] font-semibold text-xl mb-3">
-                    My Businesses
-                </h1>
-                <div className="overflow-x-auto">
-                    <table className="min-w-full divide-y divide-gray-200">
-                        <thead className="bg-gray-100">
-                            <tr className="text-gray-500">
-                                <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                    Name
-                                </th>
-                                <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                    Category
-                                </th>
-                                <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                    Details
-                                </th>
-                                <th className="px-4 py-2 text-left text-xs font-medium uppercase tracking-wider">
-                                    Amount
-                                </th>
-                                <th className="px-4 py-2 text-center text-xs font-medium uppercase tracking-wider">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody className="bg-white divide-y divide-gray-200">
-                            {business.map((item, index) => (
-                                <tr key={index}>
-                                    <td className="px-4 py-2 flex items-center">
-                                        <img
-                                            className="w-10 h-10 rounded-lg"
-                                            src={"../" + item.image}
-                                            alt="Business"
-                                        />
-                                        <div className="ml-3 text-sm">
-                                            <div className="font-medium">
-                                                {item.name}
-                                            </div>
-                                            <div>{item.contact}</div>
-                                        </div>
-                                    </td>
-                                    <td className="px-4 py-2 text-sm">
-                                        {item.category}
-                                    </td>
-                                    <td className="px-4 py-2 text-sm">
-                                        {item.details}
-                                    </td>
-                                    <td className="px-4 py-2 text-sm">
-                                        {item.amount}
-                                    </td>
-                                    <td className="px-4 py-2 text-center text-sm">
-                                        <Link
-                                            to={`/business-milestones/${btoa(
-                                                btoa(item.id)
-                                            )}`}
-                                        >
-                                            <button className="text-green-500 border border-green-500 rounded-lg py-1 px-3 text-xs">
-                                                View milestones
-                                            </button>
-                                        </Link>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </section> */}
-            {isEditModalOpen && (
-                <ServEditModal
-                    isOpen={isEditModalOpen}
-                    onClose={() => {
-                        console.log("Closing modal");
-                        setIsEditModalOpen(false);
-                    }}
-                    service={selectedService} // Pass selectedService data
-                    onUpdate={(updatedData) => {
-                        // Handle updating the service here if needed
-                    }}
-                />
-            )}
         </div>
     );
 };
