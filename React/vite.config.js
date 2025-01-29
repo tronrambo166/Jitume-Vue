25d6900f46013a19ba5448669b649c3cb90b1aa3
owen <owensmith31588@gmail.com>
2025-01-29 20:34:19 +0300

    update vite.config.js

--- React/vite.config.js
+++ React/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // commented this to bring back live site 
   // build: {
   //   chunkSizeWarningLimit: 2000, // Increase the limit
   //   rollupOptions: {
