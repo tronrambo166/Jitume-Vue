a7e3e0508182827e9dcdd105d2dc8ca78efca211
Kioko542 <kioxmanu7@gmail.com>
2025-01-29 11:56:00 +0300

    vite optimization

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            build: {
+                chunkSizeWarningLimit: 1800, // Increase the limit
+                rollupOptions: {
+                  output: {
+                    manualChunks(id) {
+                      if (id.includes("node_modules")) {
+                        return "vendor"; // Create a separate vendor chunk
+                      }
+                    },
+                  },
+                },
+              },
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
